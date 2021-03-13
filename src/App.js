aeea2f6a9b5a6508dfca9de7346c09a873f000c4
Barry Hanna <barryahanna@gmail.com>
2021-03-13 08:59:39 +0000

    Initial commit - Exercises 1-4 completed

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import CharacterEditor from "./components/CharacterEditor";
+import Footer from "./components/Footer";
+import PerspectiveBar from "./components/PerspectiveBar";
+
+function App() {
+  return (
+    <>
+      <PerspectiveBar />
+      <CharacterEditor />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/PerspectiveBar/PerspectiveBar.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./PerspectiveBar.module.css";
+
+const PerspectiveBar = () => {
+  return <div className={styles.perspectiveBar}></div>;
+};
+
+export default PerspectiveBar;
